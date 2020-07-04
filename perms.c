9f6883e2e5b90ca8f692184e0373f01af5ac6184
gavin <gaving1226@gmail.com>
2020-07-04 19:35:28 -0400

    removed unused variable

--- perms.c
+++ perms.c
@@ -5,12 +5,11 @@
 int main(){
 	char file[10];
 	char com[30] = "ls -l ";
-	char x[15] = " | cut -c 1-10";
 
 	printf("Enter a file name:\n");
 	scanf("%s", file);
 	strcat(com, file);
-	strcat(com, x);
+	strcat(com, " | cut -c 1-10");
 	printf("File (T)ype\nFile (O)wner\n(G)roup owner\n(A)ll other users\n");
 	system(com);
 	
