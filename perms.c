5fd01ebf51034220bc3b8ce3525db45839d2a1e5
gavin <gaving1226@gmail.com>
2020-07-04 19:21:59 -0400

    condensed print statements, perm types and the perms themselves of the given file, also now actually searches pwd instead of defaulting to home directory

--- perms.c
+++ perms.c
@@ -4,16 +4,15 @@
 
 int main(){
 	char file[10];
-	char com1[40] = "ls -l ~/";
-	char com2[40] = " | cut -c 1-10";
+	char com[30] = "ls -l ";
+	char x[15] = " | cut -c 1-10";
 
 	printf("Enter a file name:\n");
 	scanf("%s", file);
-
-	strcat(com1, file);
-	strcat(com1, com2);
-
-	printf("\nFirst char is file type (- indicates regular file, d indicates directory\nNext 3 chars indicate perms for file owner\nNext 3 chars indicate perms for group owner\nFinal 3 chars indicate perms for all other users\n)");
-	system(com1);
+	strcat(com, file);
+	strcat(com, x);
+	printf("File (T)ype\nFile (O)wner\n(G)roup owner\n(A)ll other users\n");
+	system(com);
+	
 	return 0;
 }
